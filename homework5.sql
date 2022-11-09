61bcd65b18461d512cc952c1777cef58a9cc2328
yaşar enes dursun <35139553+lousypickpocketraum@users.noreply.github.com>
2022-11-09 15:07:02 +0300

    Create homework5.sql

---
+++ homework5.sql
@@ -0,0 +1,11 @@
+#1
+
+select title, length from film where title '%n' order by length 'desc' limit 5;
+
+#2
+
+select title, length from film where title '%n' order by length 'asc' offset 5 limit 5;
+
+#3
+
+select last_name from customer where store_id = 1 order by last_name 'desc' limit 4;
